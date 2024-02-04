b2c32e8f0d7201c472c6971574e97935ba0a06bc
unknow <t0rjan@kali.unknow>
2024-02-04 16:27:56 +0530

    function is added and it will generate the error if any one can try to bypass it using cmd

--- zsecurity/python3/02_macchanger/usr_input_macchanger.py
+++ zsecurity/python3/02_macchanger/usr_input_macchanger.py
@@ -2,19 +2,21 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change the Mac address")
-parser.add_option("-m", "--mac", dest="new_mac", help="new Mac address")
-(options, arguments) = parser.parse_args()
-interface =options.interface
-new_mac = options.new_mac
+def get_argument():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change the Mac address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="new Mac address")
+    return parser.parse_args()
 
-print("[+] changing the macadress for " + interface + " to " + new_mac)
+def change_mac(interface, new_mac):
+    print("[+] changing the macadress for " + interface + " to " + new_mac)
 
-# subprocess.call("ifconfig "+interface+"down",shell=True)
-# subprocess.call("ifconfig"+interface+"hw ether "+new_mac,shell=True)
-# subprocess.call("ifconfig"+interface+"up",shell=True)
+    # subprocess.call("ifconfig "+interface+"down",shell=True)
+    # subprocess.call("ifconfig"+interface+"hw ether "+new_mac,shell=True)
+    # subprocess.call("ifconfig"+interface+"up",shell=True)
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+(options,arguments) = get_argument()
+change_mac(options.interface, options.new_mac)
